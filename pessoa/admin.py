600ead83a42ab389718c1d1bc24fdf52f8eaed6b
thomzera <93238590+thomzera@users.noreply.github.com>
2022-05-27 15:24:59 -0300

    Adicionando Django Admin com filtro e funções

--- pessoa/admin.py
+++ pessoa/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
+from .models import Contato, Pessoa
+
+
 # Register your models here.
+@admin.action(description="Ativar todas as pessoas")
+def ativar_todos(modeladmin, request, queryset):
+    queryset.update(ativa=True)
+
+@admin.action(description="Desativar todas as pessoas")
+def desativar_todos(modeladmin, request, queryset):
+    queryset.update(ativa=False)
+
+
+class pessoaAdmin(admin.ModelAdmin):
+    list_display = [
+        'nome_completo',
+        'data_nascimento',
+        'ativa'
+    ]
+    list_filter = [
+        'ativa',
+        'data_nascimento'
+    ]
+    search_fields = [
+        'nome_completo'
+    ]
+    actions = [
+        ativar_todos,
+        desativar_todos
+    ]
+
+
+admin.site.register(Pessoa, pessoaAdmin)
+admin.site.register(Contato)
